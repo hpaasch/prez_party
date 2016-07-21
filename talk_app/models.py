ffc18db702e8c5ff5caefac17150c010c22509b6
hpaasch <hepaasch@gmail.com>
2016-07-21 15:59:23 -0400

    HEP: twitter pull functional.

--- talk_app/models.py
+++ talk_app/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=25)
+    website = models.URLField()
+    photo = models.ImageField(upload_to='photos', null=True, blank=True)
+    affiliation = models.CharField(max_length=20)  # choices to come
+
+
+class Pundit(models.Model):
+    name = models.CharField(max_length=25)
+    website = models.URLField()
+    photo = models.ImageField(upload_to='photos', null=True, blank=True)
+    employer = models.CharField(max_length=30)
+    affiliation = models.CharField(max_length=20)  # choices to come
+
+
+class Profile(models.Model):
+    user = models.OneToOneField('auth.User')
+    friends = models.IntegerField(null=True, blank=True)
+    friend_mix = models.CharField(max_length=30, null=True, blank=True)  # choices to come
+    occupation = models.CharField(max_length=30, null=True, blank=True)
+    age = models.IntegerField(null=True, blank=True)
+    city = models.CharField(max_length=30, null=True, blank=True)
+    state = models.CharField(max_length=30, null=True, blank=True)
+    email = models.EmailField(max_length=50, null=True, blank=True)
+    registered = models.CharField(max_length=20)  # choices to come
+    affiliation = models.CharField(max_length=20)  # choices to come
+
+
+class Question(models.Model):
+    question_type = models.CharField(max_length=30)  # choices to come.
+    intensity = models.IntegerField()  # pick list of 1-5
+    wonk_level = models.IntegerField()  # pick list of 1-3
+    category = models.CharField(max_length=30)
+
+
+class DinnerParty(models.Model):
+    name = models.CharField(max_length=30)
+    host = models.ForeignKey('auth.User')
+    pundit = models.ForeignKey(Pundit)
+    candidate = models.ForeignKey(Candidate)
+
+
+class Survey(models.Model):
+    dinner = models.ForeignKey(DinnerParty)
+    host = models.ForeignKey('auth.User')
+    discussion_level = models.IntegerField()
+    change_mind = models.BooleanField()
+    made_choice = models.BooleanField()
+    top_area = models.CharField(max_length=30)  # choices to come
+
+
+class Tweet(models.Model):
+    twt_id = models.IntegerField()
+    username = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    created_at = models.TextField()
+    text = models.TextField()
+    retweet_count = models.IntegerField(null=True)
+    favorite_count = models.IntegerField(null=True)
+
+    class Meta:
+        ordering = ['-created_at']
--- talk_app/views.py
+++ talk_app/views.py
@@ -1,6 +1,45 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from TwitterAPI import TwitterAPI
+from bs4 import BeautifulSoup
+import requests
 
+from talk_app.models import Tweet
+
+tw_consumer_key = ''
+tw_consumer_secret = ''
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(request):
+        api = TwitterAPI(tw_consumer_key,
+                         tw_consumer_secret,
+                         auth_type='oAuth2')
+
+        content = api.request('statuses/user_timeline', {'screen_name': '@hillaryclinton'})
+
+        new_tweet_ids = []
+        for tweet in content:
+            new_tweet_ids.append(tweet['id'])
+
+        old_tweet_ids = []
+        old_tweets = Tweet.objects.all()
+        for tweet in old_tweets:
+            old_tweet_ids.append(tweet.twt_id)
+
+        for tweet in content:
+            if tweet['id'] not in old_tweet_ids:
+                twt_id = tweet['id']
+                username = tweet['user']['screen_name']
+                created_at = tweet['created_at']
+                text = tweet['text']
+                retweet_count = tweet['retweet_count']
+                Tweet.objects.create(twt_id=twt_id, username=username, created_at=created_at, text=text, retweet_count=retweet_count)
+
+        tweet_list = Tweet.objects.all()
+        context = {
+            'tweet_list': tweet_list,
+            }
+
+        return context
